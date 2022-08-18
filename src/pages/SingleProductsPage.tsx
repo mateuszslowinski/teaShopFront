2edc464582d8f26ca894f6d0cf8e4f7a6e1048e8
Mateusz <nowe7246@gmail.com>
2022-08-18 19:52:12 +0200

    Created SingleProductsPage with get data from global store and pass to SingleProducts.

--- src/pages/SingleProductsPage.tsx
+++ src/pages/SingleProductsPage.tsx
@@ -1,14 +1,22 @@
-import React from 'react';
-import {PageContainer} from '../constants/Layouts/PageContainer.styles';
-import {Header} from "../components/Header/Header";
+import React, {useEffect} from 'react';
 import {SingleProducts} from "../components/SingleProducts/SingleProducts";
+import {useDispatch, useSelector} from "react-redux";
+import {RootState} from "../redux/store";
+import {useParams} from "react-router";
+import { getOneProduct } from '../redux/actions/product.actions';
 
 
 export const SingleProductsPage = () => {
+    const {loading, error, product} = useSelector((store: RootState) => store.productDetails)
+    const dispatch = useDispatch();
+    const {id} = useParams();
+
+    useEffect(() => {
+        // @ts-ignore
+        dispatch(getOneProduct(id))
+    }, [dispatch, id])
+
     return (
-        <PageContainer>
-            <Header/>
-          <SingleProducts/>
-        </PageContainer>
+          <SingleProducts product={product} error={error} loading={loading}/>
     )
 }
