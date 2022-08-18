f72173e5e67166faff93883bd866c0105906a514
Mateusz <nowe7246@gmail.com>
2022-08-18 19:49:34 +0200

    Created SingleProduct component with simple styles.

--- src/components/SingleProducts/SingleProducts.styles.ts
+++ src/components/SingleProducts/SingleProducts.styles.ts
@@ -1,1 +1,87 @@
-import styled from "styled-components";
+import styled from 'styled-components'
+import {ColumnContainer, RowContainer} from "../../constants/Layouts/FlexDirection.styles";
+import {screenSize} from '../../constants/mediaQueries';
+import {appColors} from "../../constants/appColoros";
+
+export const ProductDetailsContainer = styled(RowContainer)`
+  margin: 30px auto;
+  min-height: 50vh;
+  width: 100%;
+  align-items: flex-start;
+  
+  ${screenSize.md} {
+    justify-content: space-evenly;
+    align-items: center;
+  }
+
+  & h2 {
+    background-color: transparent;
+    font-size: 2.5rem;
+    color: ${appColors.primary};
+  }
+
+  & p {
+    background-color: transparent;
+    font-size: 1.6rem;
+    color: ${appColors.primary};
+  }
+  
+  & span {
+    background-color: transparent;
+    font-size: 1.6rem;
+    color: ${appColors.primary};
+  }
+`
+
+export const ImgProduct = styled.img`
+  width: 200px;
+  height: 180px;
+
+  ${screenSize.md} {
+    width: 400px;
+    height: 300px;
+  }
+`
+
+export const DetailsContainer = styled(ColumnContainer)`
+  align-items: self-start;
+  gap: 30px;
+`
+
+export const RowDetails = styled(RowContainer)`
+  align-self: flex-start;
+  width: 100%;
+  gap: 10px;
+`
+
+
+export const StockDetails = styled(ColumnContainer)`
+  align-self: center;
+  gap: 5px;
+  padding: 10px;
+  border-radius: 10px;
+  border: 1px solid ${appColors.grandpaOrange};
+  width: 60%;
+
+  
+  & select{
+    border-radius: 4px;
+    font-size: 1.2rem;
+    border: none;
+    background-color: ${appColors.grandpaOrange};
+    color: ${appColors.primary};
+  }
+  
+  & option{
+    font-size: 1.2rem;
+    color: ${appColors.primary};
+    background-color: ${appColors.grandpaOrange};
+  }
+
+  & Button {
+    align-self: flex-end;
+    justify-self: center;
+    margin: 0 auto;
+    width: 90%;
+  }
+`
--- src/components/SingleProducts/SingleProducts.tsx
+++ src/components/SingleProducts/SingleProducts.tsx
@@ -1,8 +1,73 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { ProductTypes } from '../../types/product.types';
+import {DetailsContainer, ImgProduct, ProductDetailsContainer, RowDetails, StockDetails} from "./SingleProducts.styles";
+import img from '../../assets/tea-bag-1324303.jpg'
+import { useNavigate, useParams } from 'react-router';
+import { Button } from '../../Commons/Button/Button';
+
+interface Props {
+    product: ProductTypes
+    error: string
+    loading: boolean
+}
 
 
-export const SingleProducts =()=>{
-    return(
-        <div>single product</div>
+export const SingleProducts =  ({product, error, loading}: Props) => {
+
+    const [quantity, setQuantity] = useState<number>(1);
+    const navigate = useNavigate()
+    const {id} = useParams()
+
+    const handleAddProduct = () => {
+        console.log('klik')
+        navigate(`/koszyk/${id}?qty=${quantity}`)
+    };
+
+
+    if (product === undefined) return <p>Error.. {error}</p>
+    return (
+        <ProductDetailsContainer>
+            {loading ? (
+                <p>Wczytywanie...</p>
+            ) : (
+                <>
+                    <ImgProduct
+                        src={img}
+                        alt="product"/>
+                    <DetailsContainer>
+                        <h2>{product.name}</h2>
+                        <p>{product.description}</p>
+                        <StockDetails>
+                            <RowDetails>
+                                <p>Cena:</p>
+                                <span>{product.price} PLN</span>
+                            </RowDetails>
+                            <RowDetails>
+                                <p>Status:</p>
+                                {product.countInStock > 0 ? (
+                                    <>
+                                        <span>Dostępne</span>
+                                        <span>Ilość:</span>
+                                        <select>
+                                            {Array.from({length: product.countInStock}, (_, i) => i + 1).map((count: number) => (
+                                                <option
+                                                    key={count}
+                                                    value={count}>
+                                                    {count}
+                                                </option>)
+                                            )}
+                                        </select>
+                                        <Button text="dodaj do koszyka" onClick={handleAddProduct}/>
+                                    </>
+                                ) : (
+                                    <span>Brak w sklepie</span>
+                                )}
+
+                            </RowDetails>
+                        </StockDetails>
+                    </DetailsContainer>
+                </>
+            )}
+        </ProductDetailsContainer>
     )
 }
