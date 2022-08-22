dc729d1f130f43c55f92a068cd88670c9131742f
Mateusz <nowe7246@gmail.com>
2022-08-22 17:01:46 +0200

    The navigation links added depend on the user state. Added handle logout method.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,29 +1,55 @@
 import React from "react";
-import {BasketIcon, HeaderContainer, LinkMenu} from "./Header.style";
-import {RowContainer} from "../../constants/Layouts/FlexDirection.styles";
+import {NavLink, useNavigate} from "react-router-dom";
+import {useDispatch, useSelector} from "react-redux";
+import {RootState} from "../../redux/store";
+import {logout} from "../../redux/actions/user.actions";
 import {Button} from "../../Commons/Button/Button";
-import {NavLink} from "react-router-dom";
+import {RowContainer} from "../../constants/Layouts/FlexDirection.styles";
+import {BasketIcon, HeaderContainer, LinkMenu} from "./Header.style";
 
-export const Header = () => (
-    <HeaderContainer>
-        <ul>
-            <li>
-                <LinkMenu to="/">Strona główna</LinkMenu>
-            </li>
-            <li>
-                <LinkMenu to="produkty">Produkty</LinkMenu>
-            </li>
-        </ul>
-        <div>
-            <label>
-                <input placeholder="Szukaj..." type="text"/>
-                <Button text="szukaj"/>
-            </label>
-        </div>
-        <RowContainer>
-            <NavLink to='/rejestracja'><Button text="Rejestracja"/></NavLink>
-            <NavLink to='/zaloguj'><Button text="Logowanie"/> </NavLink>
-            <BasketIcon/>
-        </RowContainer>
-    </HeaderContainer>
-)
+
+export const Header = () => {
+    const {user} = useSelector((state: RootState) => (state.userLogin));
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        // @ts-ignore
+        dispatch(logout())
+        navigate('/')
+    }
+
+    return (
+        <HeaderContainer>
+            <ul>
+                <li>
+                    <LinkMenu to="/">Strona główna</LinkMenu>
+                </li>
+                <li>
+                    <LinkMenu to="produkty">Produkty</LinkMenu>
+                </li>
+            </ul>
+            <div>
+                <label>
+                    <input placeholder="Szukaj..." type="text"/>
+                    <Button text="szukaj"/>
+                </label>
+            </div>
+            <RowContainer>
+                {user ? (
+                    <>
+                        <NavLink to='/konto'><Button text='Moje konto'/></NavLink>
+                        <Button text='Wyloguj' onClick={handleLogout}/>
+                    </>
+                ) : (
+                    <>
+                        <NavLink to='/rejestracja'><Button text="Rejestracja"/></NavLink>
+                        <NavLink to='/zaloguj'><Button text="Logowanie"/> </NavLink>
+                    </>
+                )}
+                <NavLink to='/koszyk'><BasketIcon/></NavLink>
+            </RowContainer>
+        </HeaderContainer>
+    )
+
+}
