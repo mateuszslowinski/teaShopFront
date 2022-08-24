c6c135b9f40c3ac64644af25e2b3c1ed01d50326
Mateusz <nowe7246@gmail.com>
2022-08-24 16:36:47 +0200

    Added new types for response in user and details actions.

--- src/types/user.type.ts
+++ src/types/user.type.ts
@@ -1,8 +1,26 @@
+//USER LOGIN
+export type UserLoginDetails ={
+    _id: string,
+    isAdmin: boolean,
+    token: string,
+}
+
 export type UserLoginResponse = {
+    error:string,
+    loading: boolean
+    user: UserLoginDetails
+}
+
+
+//USER DETAILS
+export type UserDetails  = {
     _id: string,
     username: string,
     email: string,
-    isAdmin: boolean,
-    token: string,
     createdAt: Date,
+}
+export type UserDetailsResponse ={
+    error:string,
+    loading: boolean
+    user: UserDetails
 }
