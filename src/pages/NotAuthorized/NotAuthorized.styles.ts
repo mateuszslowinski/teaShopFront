594e71678c5156db426e9779b732610a43afb59a
Mateusz <nowe7246@gmail.com>
2022-09-02 18:14:34 +0200

    Created simple view not authorized page with styles.

--- src/pages/NotAuthorized/NotAuthorized.styles.ts
+++ src/pages/NotAuthorized/NotAuthorized.styles.ts
@@ -1,26 +1,26 @@
 import styled from "styled-components";
-import {appColors} from "../../constants/appColoros";
+import { appColors } from "../../constants/appColoros";
 import {ColumnContainer} from "../../constants/Layouts/FlexDirection.styles";
 
 
-export const NotFoundContainer = styled(ColumnContainer)`
+export const NotAuthorizedContainer= styled(ColumnContainer)`
   min-width: 100%;
   padding: 20px;
-  background-color: ${appColors.iceCold};
-
+  background-color: ${appColors.backColor};
+  
   & h2, h3, p, a {
     background-color: transparent;
     color: ${appColors.primary};
     font-size: 2rem;
   }
-
+  
   & h2 {
     margin-top: 80px;
     font-size: 7rem;
   }
 
   & p {
-    margin-bottom: 5px;
+    margin-bottom: 10px;
   }
 
   & a {
@@ -28,14 +28,12 @@ export const NotFoundContainer = styled(ColumnContainer)`
     text-decoration: none;
     padding: 10px;
     text-align: center;
-    background-color: ${appColors.grandpaOrange};
+    background-color: ${appColors.iceCold};
     border-radius: 10px;
     transition: 0.5s;
-    
+
     :hover {
-      background-color: ${appColors.dimWhite};
+      background-color: ${appColors.grandpaOrange};
     }
-  }
-
-
+  
 `
