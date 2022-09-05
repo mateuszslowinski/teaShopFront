2ade0a69ba50e9d5eef45ef7a155376ed102e7d1
Mateusz <nowe7246@gmail.com>
2022-09-05 15:25:37 +0200

    Fixed problem with render component.

--- src/components/UserForms/Address/Address.tsx
+++ src/components/UserForms/Address/Address.tsx
@@ -5,13 +5,12 @@ import {useDispatch, useSelector} from "react-redux";
 import {addDeliveryAddress} from "../../../redux/actions/cart.actions";
 import {RootState} from "../../../redux/store";
 import {Button} from "../../../Commons/Button/Button";
+import {ConfirmOrder} from "../../Orders/ConfirmOrder/ConfirmOrder";
 import {DeliveryType, DeliveryTypeResponse} from "../../../types/cart.types";
 import {ErrorMessage, FormContainer} from "./Address.styles";
-import {ConfirmOrder} from "../../Orders/ConfirmOrder/ConfirmOrder";
 
 
 export const Address = () => {
-
     const {deliveryAddress}: DeliveryTypeResponse = useSelector((state: RootState) => state.cart);
     const dispatch = useDispatch();
     const navigate = useNavigate();
@@ -29,72 +28,67 @@ export const Address = () => {
         navigate('/zamowienie')
     }
 
+    if (deliveryAddress) return <ConfirmOrder/>
     return (
-        <>
-            {
-                deliveryAddress ? (
-                    <FormContainer onSubmit={handleSubmit(onSubmit)} noValidate={true}>
-                        <h2>Adres dostawy:</h2>
-                        <label>
-                            <p>Imię i nazwisko:</p>
-                            <input
-                                type="text"
-                                placeholder='Imię i nazwisko'
-                                {...register('name', {
-                                    required: "Imię i nazwisko jest wymagane",
-                                })}
-                            />
-                            {name && <ErrorMessage>{name.message}</ErrorMessage>}
-                        </label>
+        <FormContainer onSubmit={handleSubmit(onSubmit)} noValidate={true}>
+            <h2>Adres dostawy:</h2>
+            <label>
+                <p>Imię i nazwisko:</p>
+                <input
+                    type="text"
+                    placeholder='Imię i nazwisko'
+                    {...register('name', {
+                        required: "Imię i nazwisko jest wymagane",
+                    })}
+                />
+                {name && <ErrorMessage>{name.message}</ErrorMessage>}
+            </label>
 
-                        <label>
-                            <p>Ulica:</p>
-                            <input
-                                type="text"
-                                placeholder='Ulica'
-                                {...register('street', {
-                                    required: "Nazwa ulicy jest wymagana",
-                                })}
-                            />
-                            {street && <ErrorMessage>{street.message}</ErrorMessage>}
-                        </label>
-                        <label>
-                            <p>Numer mieszkania:</p>
-                            <input
-                                type="text"
-                                placeholder='Number mieszkania'
-                                {...register('buildingNumber', {
-                                    required: "Numer mieszkania jest wymagany",
-                                })}
-                            />
-                            {buildingNumber && <ErrorMessage>{buildingNumber.message}</ErrorMessage>}
-                        </label>
-                        <label>
-                            <p>Kod pocztowy</p>
-                            <input
-                                type="text"
-                                placeholder='Kod pocztowy'
-                                {...register('zipCode', {
-                                    required: "Kod pocztowy jest wymagany",
-                                })}
-                            />
-                            {zipCode && <ErrorMessage>{zipCode.message}</ErrorMessage>}
-                        </label>
-                        <label>
-                            <p>Miasto</p>
-                            <input
-                                type="text"
-                                placeholder='Miasto'
-                                {...register('city', {
-                                    required: "Miasto jest wymagane",
-                                })}
-                            />
-                            {city && <ErrorMessage>{city.message}</ErrorMessage>}
-                        </label>
-                        <Button text="Potwierdź"/>
-                    </FormContainer>
-                ) : <ConfirmOrder/>
-            }
-        </>
+            <label>
+                <p>Ulica:</p>
+                <input
+                    type="text"
+                    placeholder='Ulica'
+                    {...register('street', {
+                        required: "Nazwa ulicy jest wymagana",
+                    })}
+                />
+                {street && <ErrorMessage>{street.message}</ErrorMessage>}
+            </label>
+            <label>
+                <p>Numer mieszkania:</p>
+                <input
+                    type="text"
+                    placeholder='Number mieszkania'
+                    {...register('buildingNumber', {
+                        required: "Numer mieszkania jest wymagany",
+                    })}
+                />
+                {buildingNumber && <ErrorMessage>{buildingNumber.message}</ErrorMessage>}
+            </label>
+            <label>
+                <p>Kod pocztowy</p>
+                <input
+                    type="text"
+                    placeholder='Kod pocztowy'
+                    {...register('zipCode', {
+                        required: "Kod pocztowy jest wymagany",
+                    })}
+                />
+                {zipCode && <ErrorMessage>{zipCode.message}</ErrorMessage>}
+            </label>
+            <label>
+                <p>Miasto</p>
+                <input
+                    type="text"
+                    placeholder='Miasto'
+                    {...register('city', {
+                        required: "Miasto jest wymagane",
+                    })}
+                />
+                {city && <ErrorMessage>{city.message}</ErrorMessage>}
+            </label>
+            <Button text="Potwierdź"/>
+        </FormContainer>
     )
 }
