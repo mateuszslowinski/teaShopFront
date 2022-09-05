f863fd65ae9e9a8ee42d8f15a507d0a33ba1deec
Mateusz <nowe7246@gmail.com>
2022-09-05 14:21:52 +0200

    Added pagination to products results.

--- src/components/Products/Products.styles.ts
+++ src/components/Products/Products.styles.ts
@@ -1,17 +1,19 @@
 import styled from "styled-components";
-import {RowContainer} from "../../constants/Layouts/FlexDirection.styles";
+import { RowContainer} from "../../constants/Layouts/FlexDirection.styles";
 import {appColors} from "../../constants/appColoros";
 
 
 export const ProductsSectionContainer = styled(RowContainer)`
-    
-    & h1 {
-      width: 100%;
-      text-align: center;
-      margin-top: 20px;
-      font-size: 2.5rem;
-      background-color: transparent;
-      color: ${appColors.primary};
-    }
-    
+  margin-bottom: 20px;
+  width: 100%;
+
+  
+  & h1 {
+    width: 100%;
+    text-align: center;
+    margin-top: 20px;
+    font-size: 2.5rem;
+    background-color: transparent;
+    color: ${appColors.primary};
+  }
 `
