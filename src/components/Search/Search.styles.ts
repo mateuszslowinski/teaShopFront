6ce386ee2a4b6197e32f43990ba26c9bc36c68ed
Mateusz <nowe7246@gmail.com>
2022-09-05 11:34:12 +0200

    Added showing text information for searching term.

--- src/components/Search/Search.styles.ts
+++ src/components/Search/Search.styles.ts
@@ -3,10 +3,15 @@ import {RowContainer} from "../../constants/Layouts/FlexDirection.styles";
 import {appColors} from "../../constants/appColoros";
 
 export const SearchContainer = styled(RowContainer)`
-
-
+& h2 {
+    width: 100%;
+    text-align: center;
+    margin-top: 20px;
+    font-size: 2.5rem;
+    background-color: transparent;
+    color: ${appColors.primary};
+}
 `
-
 export const SearchMessage = styled.div`
   font-size: 2rem;
   color: ${appColors.primary};
--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,27 +1,25 @@
 import React, {useEffect, useState} from 'react';
-import {ProductTypes} from "../../types/product.types";
-import {SingleProductCard} from "../Products/SingleProductCard/SingleProductCard";
-import {useParams} from "react-router-dom";
 import {useDispatch, useSelector} from "react-redux";
-import {RootState} from "../../redux/store";
-import {LoadingSpinner} from "../../Commons/LoadingSpinner/LoadingSpinner";
 import {getListProducts} from "../../redux/actions/product.actions";
+import {RootState} from "../../redux/store";
+import {useParams} from "react-router-dom";
+import {SingleProductCard} from "../Products/SingleProductCard/SingleProductCard";
+import {LoadingSpinner} from "../../Commons/LoadingSpinner/LoadingSpinner";
+import {ProductTypes} from "../../types/product.types";
 import {SearchContainer, SearchMessage} from "./Search.styles";
-
 
 
 export const Search = () => {
     const dispatch = useDispatch();
     const {term} = useParams();
-    const [filterProducts, setFilterProducts] = useState([]);
+    const [filterProducts, setFilterProducts] = useState<ProductTypes[]>([]);
     const {loading, error, products} = useSelector((store: RootState) => store.productList)
 
     const search = () => {
         // @ts-ignore
         dispatch(getListProducts())
-        const newProducts = (products.filter(product => product.name.includes(term as string)));
-        // @ts-ignore
-        setFilterProducts(newProducts)
+        const newProducts = (products.filter(product => product.name.includes(term as string)))
+            setFilterProducts(newProducts)
     }
 
     useEffect(() => {
@@ -31,6 +29,7 @@ export const Search = () => {
     if (filterProducts.length === 0) return <SearchMessage>Brak wyników wyszukiwania!</SearchMessage>
     return (
         <SearchContainer>
+            <h2>Wyniki wyszukiwania dla {term}:</h2>
             {loading
                 ? <LoadingSpinner/>
                 : error
@@ -43,4 +42,3 @@ export const Search = () => {
         </SearchContainer>
     )
 }
-// )
