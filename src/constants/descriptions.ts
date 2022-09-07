fe7635aa57eb493f64133aab61fbcbcb72a001b5
Mateusz <nowe7246@gmail.com>
2022-09-07 14:07:14 +0200

    Created constants description for product category page.

---
+++ src/constants/descriptions.ts
@@ -0,0 +1,32 @@
+export const categoryProductDesc = [
+    {
+    title: 'biała herbata',
+    text: "biała herbata jest produkowana z pąków i młodych liści wiecznie zielonego krzewu herbacianego –" +
+        " Camellia sinensis. W zależności od miejsca produkcji różnie określa się jej charakterystykę. Większość właściwości białej herbaty pokrywa się z właściwościami herbat zielonej, czarnej czy czerwonej. Podobnie jak one zawiera antyoksydanty – głównie polifenole, garbniki oraz kofeinę (nazywaną również teiną).  \n" +
+        "\n" +
+        "W wielu źródłach można znaleźć informacje, że biała herbata jest uboższa w kofeinę oraz bogatsza w antyoksydanty w porównaniu z herbatą zieloną. Istnieją również badania, w których w wyniku porównań składu chemicznego herbat powyższe doniesienia są niepotwierdzane. Ta rozbieżność może mieć wiele przyczyn. Główną jest fakt, że w przypadku każdej próbki naturalnej poziom zawartych w niej składników zależy od wielu czynników, np. rodzaju gleby, nasłonecznienia, czasu zbioru, wilgotności, sposobu przechowywania. Kolejna to jakość wybranych do badań herbat. Nie jest tajemnicą, że produkty spożywcze często są fałszowane przez nieuczciwych producentów. W przypadku herbaty białej, która ze względu na ograniczoną ilość surowca (najmłodsze pędy i liście) należy do najdroższych herbat, również raportowane są przypadki zafałszowań.  "
+
+},
+    {
+        title: 'czarna herbata',
+        text: "Czarną herbatę (jak i inne jej odmiany, np. herbatę białą i herbatę zieloną) pozyskuje się z młodych liści, pączków liściowych lub delikatnych łodyżek krzewu Camellia sinensis: z odmiany chińskiej (uprawy znajdują się głównie w Chinach, Japonii i Tybecie) albo assamskiej (uprawianej w Indiach, Birmie, na Półwyspie Indochińskim). Alkaloidy purynowe zawarte w herbacie (głównie kofeina, zwana tu teiną) determinują jej działanie pobudzające na układ nerwowy: zmniejsza się senność i zmęczenie, usprawnione są procesy myślowe, poprawia się nastrój. Wzmocniona jest akcja serca i wentylacja płuc. "
+
+    },
+    {
+        title: 'owocowa herbata',
+        text: "Herbata owocowa nie jest herbatą w dosłownym tego słowa znaczenia. Klasyczne, dobrej jakości herbaty owocowe nie zawierają bowiem liści herbaty, a jedynie susz z owoców lub ich suszone kawałki.Herbata owocowa nie zawiera kofeiny, zatem w odróżnieniu od tradycyjnej herbaty nie ma działania pobudzającego. Można ją pić na ciepło - wtedy rozgrzewa, lub na zimno - po schłodzeniu znakomicie orzeźwia.Herbata owocowa ma również właściwości zdrowotne, które zależą od tego, który rodzaj suszu lub suszonych kawałków owoców dominuje w jej składzie. Może działać przeciwzapalnie, napotnie (np. herbata malinowa), wspierać pracę układu moczowego, poprawiać odporność, a nawet obniżać cholesterol.  "
+
+    },
+    {
+        title: 'yerba mate',
+        text: "Yerba to naturalny napar przyrządzany z liści ostrokrzewu paragwajskiego (ilex paraguariensis," +
+            " St.Hillaire). Napój ten ma niezwykłe właściwości pobudzające, wzmacniające, oczyszczające i regulujące. Skład Yerba Mate jest bogaty w składniki odżywcze, dzięki czemu spektrum jej dobroczynnego działania na organizm jest fantastycznie szerokie. Prozdrowotne właściwości Yerba Mate są widoczne już na pierwszy rzut oka. Po pierwsze napar z Ostrokrzewu Paragwajskiego wykazuje właściwości antyoksydacyjne, czyli krótko mówiąc pomaga nam w oczyszczeniu organizmu z szkodliwych wolnych rodników, które mogą powodować przyspieszenie procesów starzenia się skóry i całego ciała oraz mogą zwiększać ryzyko występowania licznych chorób (uszkadzając łańcuchy DNA). Dodatkowo Mate uznawana jest za produkt moczopędny, przez co dodatkowo pomaga usuwać toksyny z organizmu. "
+
+    },
+    {
+        title: 'zielona herbata',
+        text: "Zielona herbata przebyła długą i fascynująca drogę, zanim znalazła się na europejskich stołach. Jej dobroczynny wpływ na nastrój oraz samopoczucie jako pierwsi odkryli Chińczycy. Dziś napar ze słabo sfermentowanych listków piją ludzie na całym świecie. Doceniają nie tylko walory smakowe, ale również wspaniałe prozdrowotne właściwości zielonej herbaty. Zielona herbata powstaje z listków poddanych minimalnemu procesowi utlenienia. Dzięki temu zawiera o wiele więcej składników mineralnych niż choćby herbata czarna. Przyrządzony z niej napar ma również znacznie bardziej delikatny smak i aromat oraz subtelną barwę. Istnieją różne herbaty zielone. Ich gatunki i rodzaje wyróżnia się na podstawie kraju pochodzenia, warunków uprawy czy pory zbiorów. Uwagę zwraca się również na wygląd liści, a także kolor i smak przyrządzonego z nich naparu. Choć zielona herbata po raz pierwszy zaczęła być parzona w Chinach, szybko rozprzestrzeniła się na inne rejony świata. Już od wielu lat z wielkim powiedzeniem uprawia się ją również w Japonii, Indiach, Indonezji, Wietnamie na Sri Lance czy w Kenii. "
+
+    },
+
+]
