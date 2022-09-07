28ec471544ebc12b2002a80f160d0c18e140b16b
Mateusz <nowe7246@gmail.com>
2022-09-07 14:53:42 +0200

    Added new constants description for home page.

---
+++ src/constants/descriptions/homePage.descriptions.ts
@@ -0,0 +1,6 @@
+export const AboutShopDesc  = 'Witaj w naszym sklepie z herbatami, gdzie staram się o jak najlepsze jakość' +
+    ' produkty. Jesteśmy specjalistyczną firmą,dzięki temu, że współpracujemy z kilkudziesięcioma dostawcami - od' +
+    ' producentów herbaty, plantatorów w Chinach, Nepalu, Japonii, na Tajwanie, po wiodące hurtownie europejskie.' +
+    ' Odwiedziliśmy osobiście wiele plantacji herbacianych w wielu krajach i chętnie dzielimy się wiedzą o herbacie.' +
+    ' Najbardziej wrażliwe herbaty zielone przechowujemy w warunkach chłodniczych.Nasze produkty są zawsze przez nas' +
+    ' testowane, a następnie starannie przechowywane z maksymalną troską. Zapraszamy do zapoznania się z naszą ofertą.'
