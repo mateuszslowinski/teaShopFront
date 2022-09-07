cbaa8dc6a41c59bb67fa2ba8232c4eb249db2fe9
Mateusz <nowe7246@gmail.com>
2022-09-07 14:17:33 +0200

    Created constants for images of slider on home page.

---
+++ src/constants/images/images.ts
@@ -0,0 +1,19 @@
+export const ImagesForHomeSlides = [
+    {
+        url:'../images/home/1.jpg',
+        title:'Nasze produkty'
+    },
+    {
+        url:'../images/home/2.jpg',
+        title:'jakis tytul'
+    },
+    {
+        url:'../images/home/3.jpg',
+        title:'Do pomyslenia'
+    },
+    {
+        url:'../images/home/4.jpg',
+        title:'Nasze produkty'
+    },
+
+]
